a411ffe189050f0b0dad6abc5e439b1db0f1c361
David Cain <d.cain2740@gmail.com>
2020-09-25 16:59:04 -0500

    creating boot image

--- PynqSoftware/Projects/Hello_World/Hello_World.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ PynqSoftware/Projects/Hello_World/Hello_World.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
-//Date        : Tue Sep 15 15:19:54 2020
+//Date        : Fri Sep 25 16:42:26 2020
 //Host        : DESKTOP-D9F9TPQ running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
