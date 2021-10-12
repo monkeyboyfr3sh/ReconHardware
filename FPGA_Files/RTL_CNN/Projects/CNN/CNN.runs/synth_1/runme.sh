4dd6e896442efb5bae529c038deea5ecda4364a0
David <d.cain2740@gmail.com>
2021-10-12 09:58:12 -0500

    This is a working point for CNN framework

--- FPGA_Files/RTL_CNN/Projects/CNN/CNN.runs/synth_1/runme.sh
+++ FPGA_Files/RTL_CNN/Projects/CNN/CNN.runs/synth_1/runme.sh
@@ -11,9 +11,9 @@ echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executi
 exit
 
 if [ -z "$PATH" ]; then
-  PATH=D:/Xilinx/Vitis/2020.1/bin;D:/Xilinx/Vivado/2020.1/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2020.1/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2020.1/bin
+  PATH=C:/Xilinx/Vitis/2020.1/bin;C:/Xilinx/Vivado/2020.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2020.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2020.1/bin
 else
-  PATH=D:/Xilinx/Vitis/2020.1/bin;D:/Xilinx/Vivado/2020.1/ids_lite/ISE/bin/nt64;D:/Xilinx/Vivado/2020.1/ids_lite/ISE/lib/nt64:D:/Xilinx/Vivado/2020.1/bin:$PATH
+  PATH=C:/Xilinx/Vitis/2020.1/bin;C:/Xilinx/Vivado/2020.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2020.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2020.1/bin:$PATH
 fi
 export PATH
 
